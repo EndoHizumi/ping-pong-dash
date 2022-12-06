89cb3bc28b538a1e2c08a52d4587f76d701b50df
endohizumi <endohizumi0114@gmail.com>
2022-12-06 19:51:39 +0900

    リアクションを追加した

--- src/components/game.vue
+++ src/components/game.vue
@@ -1,20 +1,31 @@
 <template>
     <div id="game">
         <div>ピンポンした数：{{ count }}</div>
+        <div class="door">
+            <img :src="doorImagePath">
+        </div>
         <div>
-            <img @click="play" class="doorBell" src="@/assets/door_bell.png" />
+            <img @click="call" class="doorBell" src="@/assets/door_bell.png" />
         </div>
     </div>
 </template>
 <script>
+let timeoutId=null
 let audioBuffer = null
+const doorImageList = {
+                close:require('@/assets/door_close.png'),
+                halfOpen:require('@/assets/door_half_open.png'),
+                police:require('@/assets/job_police_man.png')
+            }
 export default {
     name: "gameVue",
     data() {
         return {
             context: new AudioContext(),
             source: null,
-            count: 0
+            count: 0,
+            isWatch: false,
+            doorImagePath: doorImageList.close
         }
     },
     async mounted() {
@@ -23,8 +34,19 @@ export default {
         audioBuffer = await this.context.decodeAudioData(arrBuffer)
     },
     methods: {
+        call() {
+            this.play()
+            this.isWatch = true
+            clearTimeout(timeoutId)
+            timeoutId = setTimeout(() => {
+                this.doorImagePath = doorImageList.halfOpen
+                setTimeout(() => {
+                    this.doorImagePath = doorImageList.close
+                }, 1000);
+            }, 3000);
+        },
         play() {
-            if(this.source && this.context.state=='running'){
+            if (this.source && this.context.state == 'running') {
                 this.source.stop()
             }
             this.source = this.context.createBufferSource()
@@ -32,6 +54,20 @@ export default {
             this.source.connect(this.context.destination)
             this.source.start()
             this.count++
+        }
+    },
+    watch: {
+        isWatch() {
+            if (this.isWatch) {
+                setTimeout(() => {
+                    if (this.count >= 50) {
+                        clearTimeout(timeoutId)
+                        this.doorImagePath = doorImageList.police
+                        this.count = 0
+                    }   
+                    this.isWatch = false
+                }, 10000);
+            }
         }
     }
 }
@@ -44,5 +80,4 @@ export default {
 .doorBell:active {
     transform: translateY(5px);
 }
-
 </style>
