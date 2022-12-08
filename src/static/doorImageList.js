41680683f5b82eca4bb4a02420735ac03a62eabc
EndoHizumi <endohizumi0114@gmail.com>
2022-12-09 01:12:01 +0900

    適切な定義に変更した

--- src/static/doorImageList.js
+++
@@ -1,9 +0,0 @@
-const doorImageList = {
-    close: require('@/assets/door_close.png'),
-    halfOpen: require('@/assets/door_half_open.png'),
-    police: require('@/assets/job_police_man.png'),
-    cardboardOpen: require('@/assets/cardboard_open.png'),
-    cardboardClose: require('@/assets/cardboard.png')
-}
-
-export { doorImageList }
---
+++ src/static/imageList.js
@@ -0,0 +1,13 @@
+const doorImageList = {
+    close: require('@/assets/door_close.png'),
+    halfOpen: require('@/assets/door_half_open.png'),
+    police: require('@/assets/job_police_man.png'),
+    noticed: require('@/assets/door_half_open_noticed.png')
+}
+
+const hideImageList = {
+    hide: require('@/assets/hiding.png'),
+    empty: require('@/assets/empty.png')
+}
+
+export { doorImageList, hideImageList }
